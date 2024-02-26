c3e3b0f6c0536befeb15f172fa8f155f8c26f2d0
jannes <jannesdrijkoningen@gmail.com>
2024-02-26 13:01:34 +0100

    add users from secondary assignments to roster

--- src/Forum/Controller/RosterController.php
+++ src/Forum/Controller/RosterController.php
@@ -17,11 +17,40 @@ class RosterController extends AbstractController
         $perscom = $perscomFactory->getPerscom();
         $groups = $perscom
             ->groups()
-            ->all(['units', 'units.users', 'units.users.position', 'units.users.rank', 'units.users.rank.image', 'units.users.status'])
+            ->all([
+                'units',
+                'units.users',
+                'units.users.position',
+                'units.users.rank',
+                'units.users.rank.image',
+                'units.users.status',
+                'units.secondary_assignment_records',
+                'units.secondary_assignment_records.position',
+                'units.secondary_assignment_records.user',
+                'units.secondary_assignment_records.user.rank',
+                'units.secondary_assignment_records.user.rank.image',
+                'units.secondary_assignment_records.user.status',
+            ])
             ->json('data') ?? [];
 
         return $this->render('@ForumifyPerscomPlugin/frontend/roster/roster.html.twig', [
-            'groups' => $groups
+            'groups' => array_map($this->mergeSecondaryUnitsIntoPrimary(...), $groups),
         ]);
+    }
+
+    private function mergeSecondaryUnitsIntoPrimary(array $group): array
+    {
+        foreach ($group['units'] as &$unit) {
+            foreach ($unit['secondary_assignment_records'] ?? [] as $secondaryAssignment) {
+                if (empty($secondaryAssignment['user'])) {
+                    continue;
+                }
+
+                $secondaryAssignment['user']['position'] = $secondaryAssignment['position'] ?? ['name' => ''];
+                $unit['users'][] = $secondaryAssignment['user'];
+            }
+        }
+
+        return $group;
     }
 }
