07bcc0b4fc78ca7266eac174d07c1aa3f3325ef6
Jon Erickson <jon@deschutesdesigngroup.com>
2024-09-29 09:08:42 -0700

    Make PERSCOM ID optional

--- src/Admin/Form/SettingsType.php
+++ src/Admin/Form/SettingsType.php
@@ -18,6 +18,11 @@ class SettingsType extends AbstractType
             ->add('perscom__endpoint', TextType::class, [
                 'label' => 'Endpoint',
             ])
+            ->add('perscom__perscom_id', TextType::class, [
+                'label' => 'PERSCOM ID',
+                'required' => false,
+                'help' => 'Can be found on your PERSCOM dashboard. This is not required.',
+            ])
             ->add('perscom__api_key', PasswordType::class, [
                 'label' => 'API Key',
                 'help' => 'Create a new API key for forumify on your PERSCOM dashboard, under "Integrations" > "API Keys". Select "All scopes" to allow the API key to access every resource.',
