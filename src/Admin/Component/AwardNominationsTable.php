fd9bc942211643396db5893f85989673d2521c32
Goltred <goltred@hotmail.com>
2025-05-16 15:43:41 -0400

    Renamed awardnominations folder Removed some columns from AwardNominationsTable Replaced set methods for constructor injection in AwardNominationsTable

--- src/Admin/Component/AwardNominationsTable.php
+++ src/Admin/Component/AwardNominationsTable.php
@@ -16,35 +16,14 @@ use Twig\Environment;
 #[AsLiveComponent('AwardNominationsTable', '@ForumifyPerscomPlugin/frontend/components/award_nominations_table.html.twig')]
 class AwardNominationsTable extends AbstractTable
 {
-    private PerscomFactory $perscomFactory;
-    private AwardNominationRepository $awardNominationRepository;
     private ?array $data = null;
 
-    #[Required]
-    public function setPerscomFactory(PerscomFactory $perscomFactory): void
-    {
-        $this->perscomFactory = $perscomFactory;
-    }
-
-    #[Required]
-    public function setAwardNominationRepository(AwardNominationRepository $awardNominationRepository): void
-    {
-        $this->awardNominationRepository = $awardNominationRepository;
-    }
-
-    protected TranslatorInterface $translator;
-    protected Environment $twig;
-
-    #[Required]
-    public function setTranslator(TranslatorInterface $translator): void
-    {
-        $this->translator = $translator;
-    }
-
-    #[Required]
-    public function setTwig(Environment $twig): void
-    {
-        $this->twig = $twig;
+    public function __construct(
+        private readonly PerscomFactory $perscomFactory,
+        private readonly AwardNominationRepository $awardNominationRepository,
+        private readonly TranslatorInterface $translator,
+        private readonly Environment $twig
+    ) {
     }
 
     protected function buildTable(): void
@@ -54,38 +33,26 @@ class AwardNominationsTable extends AbstractTable
                  'field' => 'nomination.createdDate',
                  'sortable' => false,
                  'searchable' => false,
-                 'class' => 'w-10 text-small',
+                 'class' => 'w-10',
                  'renderer' => fn (DateTime $date) => $this->translator->trans('date', ['date' => $date]),
              ])
-            ->addColumn('requester', [
-                'field' => 'requesterItem[name]',
-                'sortable' => false,
-                'searchable' => false,
-                'class' => 'text-left text-small'
-            ])
             ->addColumn('receiver', [
                 'field' => 'receiverItem[name]',
                 'sortable' => false,
                 'searchable' => false,
-                'class' => 'text-left text-small',
+                'class' => 'text-left',
             ])
             ->addColumn('award', [
                 'field' => 'awardItem[name]',
                 'sortable' => false,
                 'searchable' => false,
-                'class' => 'text-left text-small',
-            ])
-            ->addColumn('reason', [
-                'field' => 'nomination.reason',
-                'sortable' => false,
-                'searchable' => false,
-                'class' => 'text-left text-small',
+                'class' => 'text-left',
             ])
             ->addColumn('status', [
                 'field' => 'statusItem',
                 'sortable' => false,
                 'searchable' => false,
-                'class' => 'text-left text-small',
+                'class' => 'text-left',
                 'renderer' => fn ($status) => $status !== null
                     ? $this->twig->render('@ForumifyPerscomPlugin/frontend/roster/components/status.html.twig', [
                         'class' => 'text-small',
@@ -93,15 +60,9 @@ class AwardNominationsTable extends AbstractTable
                     ])
                     : '',
             ])
-            ->addColumn('reviewed by', [
-                'field' => 'updatedByItem?[name]',
-                'sortable' => false,
-                'searchable' => false,
-                'class' => 'text-left text-small',
-            ])
             ->addColumn('actions', [
                 'label' => '',
-                'renderer' => fn ($_, $row) => $this->twig->render('@ForumifyPerscomPlugin/admin/awardnominations/actions.html.twig', [
+                'renderer' => fn ($_, $row) => $this->twig->render('@ForumifyPerscomPlugin/admin/award_nominations/actions.html.twig', [
                     'data' => $row,
                 ]),
                 'searchable' => false,
