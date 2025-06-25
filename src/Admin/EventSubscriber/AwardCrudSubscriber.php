593fdd009c9ec84ac20021a48a8f34060a4bdc67
Oscar Ellis <oscarx1be@gmail.com>
2025-06-25 22:47:17 +0100

    added award crud

---
+++ src/Admin/EventSubscriber/AwardCrudSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Forumify\PerscomPlugin\Admin\EventSubscriber;
+
+use Forumify\Admin\Crud\Event\PreSaveCrudEvent;
+use Forumify\Core\Service\MediaService;
+use Forumify\PerscomPlugin\Perscom\Entity\Award;
+use League\Flysystem\FilesystemOperator;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class AwardCrudSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private readonly MediaService $mediaService,
+        private readonly FilesystemOperator $perscomAssetStorage,
+    ) {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [PreSaveCrudEvent::getName(Award::class) => 'preSaveAward'];
+    }
+
+    public function preSaveAward(PreSaveCrudEvent $event): void
+    {
+        $award = $event->getEntity();
+        $form = $event->getForm();
+        $newImage = $form->get('newAwardImage')->getData();
+        if ($newImage instanceof UploadedFile) {
+            $image = $this->mediaService->saveToFilesystem($this->perscomAssetStorage, $newImage);
+            $award->setImage($image);
+        }
+    }
+}
