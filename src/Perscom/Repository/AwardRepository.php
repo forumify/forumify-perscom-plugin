b88d913d5a734ee7bbeebf44cb3a7232af3f060e
Oscar Ellis <oscarx1be@gmail.com>
2025-06-26 14:00:40 +0100

    added phpdoc to class

--- src/Perscom/Repository/AwardRepository.php
+++ src/Perscom/Repository/AwardRepository.php
@@ -6,6 +6,9 @@ namespace Forumify\PerscomPlugin\Perscom\Repository;
 
 use Forumify\PerscomPlugin\Perscom\Entity\Award;
 
+/**
+ * @extends AbstractPerscomRepository<Award>
+ */
 class AwardRepository extends AbstractPerscomRepository
 {
     public static function getEntityClass(): string
